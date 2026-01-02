8dbe202232fa914cca158b2cf814122efed9b1eb
BenziDarwin <ssalibenjamin0402@gmail.com>
2026-01-02 18:08:59 +0300

    Created boolean

--- casting.py
+++ casting.py
@@ -13,4 +13,12 @@ This is to change one datatype to another.
 number
 age = float(age)
 print(age)
-print(type(age))
+print(type(age))   
+
+ans = input("Are you okay?")
+
+print(type(bool(ans)))
+print(bool(ans))
+
+# False = 0
+# True = 1
