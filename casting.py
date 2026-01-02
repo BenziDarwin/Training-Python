ae8b2c10541767730d29afadacb3c0cb77253f68
BenziDarwin <ssalibenjamin0402@gmail.com>
2026-01-02 17:52:54 +0300

    Added casting file

---
+++ casting.py
@@ -0,0 +1,16 @@
+age:str = input("What is your age?")
+print(type(age))
+print(age)
+
+number = 12
+
+print("A number is " + str(number)) 
+
+"""
+What is casting?
+This is to change one datatype to another.
+"""
+number
+age = float(age)
+print(age)
+print(type(age))
